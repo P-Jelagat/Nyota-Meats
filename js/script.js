cb160cf69e29034d7d9c1061894591d03176210d
Patience Jelagat <patiencejelagatt@gmail.com>
2026-06-02 18:52:44 +0300

    Styling complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+const menuBtn = document.querySelector('#menu-btn');
+const menu = document.querySelector('.menu');
 
+console.log(menuBtn);
+console.log(menu);
+
+menuBtn.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    menu.classList.toggle('show');
+    
+});
+
+document.addEventListener('click', (e) => {
+ const clickedInsideMenu = menu.contains(e.target);
+ const clickedButton = menuBtn.contains(e.target);
+
+ if(!clickedInsideMenu && !clickedButton){
+
+    menu.classList.remove('show');
+
+}
+});
